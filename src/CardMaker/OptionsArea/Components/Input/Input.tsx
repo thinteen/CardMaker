a4df6cfcb69958e1df7be80ec4802fbc54d65209
thinteen <80061280+thinteen@users.noreply.github.com>
2022-01-25 13:07:40 +0300

    Delete src directory

--- src/CardMaker/OptionsArea/Components/Input/Input.tsx
+++
@@ -1,9 +0,0 @@
-import style from './Input.module.css';
-
-function Input() {
-  return (
-    <input className={style.input}></input>
-  );
-}
-
-export default Input;
--- src/CardMaker/OptionsArea/Components/InputColor/InputColor.tsx
+++
@@ -1,9 +0,0 @@
-import style from './InputColor.module.css';
-
-function InputColor() {
-  return (
-    <input type="color" className={style.inputColor}></input>
-  );
-}
-
-export default InputColor;
